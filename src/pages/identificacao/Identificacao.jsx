9b1ff1593ff991163bd7e238a589c079170a9538
Danilo Herculano <64225977+danilolan@users.noreply.github.com>
2022-02-04 21:44:52 -0300

    Added page identificacao

--- src/pages/identificacao/Identificacao.jsx
+++ src/pages/identificacao/Identificacao.jsx
@@ -1,12 +1,66 @@
-import React from 'react';
+import React, {useState} from 'react';
+import { useNavigate } from "react-router";
 
 //---STYLES---
 import './identificacao.scss'
 
+//---COMPONENTS---
+import Input from '../../components/input/Input';
+import Button from '../../components/button/Button';
+
+//---MASKS---
+import emailMask from '../../services/masks/email'
+
+//---CONTEXT---
+import { useData } from '../../context/data'
+
 function Identificacao() {
+    const navigate = useNavigate();
+    const { data, setData } = useData()
+
+    const [email, setEmail] = useState('');
+    const [emailError, setEmailError] = useState(false);
+
+    function next(e){
+        e.preventDefault()
+        if(emailMask(email)){
+            let editData = data
+            editData.email = email
+            setData(editData)
+            navigate("/dados-pessoais", { replace: true });
+        }
+        else{
+            setEmailError(true)
+        }
+    }
+
     return ( 
         <div className="identificacao">
-            Identificacao
+            <div className="title"> O cadastro é rapidinho </div>
+            <form>
+
+                <div className="row"> 
+                    <Input
+                        label='E-mail'
+                        value={email}
+                        setValue={setEmail}
+                        width='500px'
+                        error={emailError}
+                    />
+                </div>
+
+                <div className="hr-bar"/>
+
+                <div className="row">
+                    <Button
+                        isFill={true}
+                        width='300px'
+                        click={next}
+                    >
+                        Continuar
+                    </Button>
+                </div>
+            </form>
         </div>
      );
 }

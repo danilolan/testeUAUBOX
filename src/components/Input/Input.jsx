b12c1d5f5bc8b8cf6b040af55f0e08d781568181
Danilo Herculano <64225977+danilolan@users.noreply.github.com>
2022-02-04 21:08:58 -0300

    Added width prop input

--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -3,11 +3,16 @@ import React from 'react';
 //---STYLES---
 import './input.scss'
 
-function Input({ label, value, setValue, error }) {
+function Input({ label, value, setValue, error, width }) {
     return ( 
         <div className="input">
             <div className="label">{label}</div>
-            <input className={error ? 'error' : ''} type="text" value={value} onChange={ e => setValue(e.target.value)} />
+            <input 
+                className={error ? 'error' : ''} 
+                type="text" value={value} 
+                onChange={ e => setValue(e.target.value)} 
+                style={width ? {width} : {width:'300px'}}
+            />
         </div>
      );
 }

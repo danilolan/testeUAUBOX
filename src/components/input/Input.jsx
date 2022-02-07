0593dd621ccfb6bdebaa18ce458deb9626b8f5d0
Danilo Herculano <64225977+danilolan@users.noreply.github.com>
2022-02-07 17:14:22 -0300

    input folder rename

---
+++ src/components/input/Input.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+//---STYLES---
+import './input.scss'
+
+//---COMPONENTS---
+import InputMask from 'react-input-mask';
+
+function Input({ label, value, setValue, error, width, type, mask, maskChar }) {
+    return ( 
+        <div className="input" style={width ? {width} : {width:'300px'}}>
+            <div className="label">{label}</div>
+            <InputMask 
+                className={error ? 'error' : ''} 
+                type="text" 
+                value={value} 
+                onChange={ e => setValue(e.target.value)} 
+                name={type}
+                mask={mask}
+                maskChar={maskChar}
+            />
+        </div>
+     );
+}
+
+export default Input;
